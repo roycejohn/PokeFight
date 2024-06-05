2b43c0715948a4d937995ec71ce932b2115b833e
roycejohn <cuevasroycejohn.com>
2024-06-05 14:30:13 +0200

    Pages renamed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,11 +3,11 @@ import './App.css'
 import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
-import Home from './pages/Home'
-import PokeList from './pages/PokeList'
+import Home from './Pages/Home'
+import PokeList from './Pages/PokeList'
 import PokeDetails from './components/PokeDetails'
-import About from './pages/About'
-import Game from './pages/Game'
+import About from './Pages/About'
+import Game from './Pages/Game'
 
 
 
