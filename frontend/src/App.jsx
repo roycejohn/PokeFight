b6b9aa4a8845285aa51ae2628f773e3abef61214
Denis Sab <denis.sabs@ymail.com>
2024-06-05 10:04:59 +0200

    merging debuging

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,10 @@
 
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Home from './pages/Home'
 import PokeList from './pages/PokeList'
 import PokeDetails from './components/PokeDetails'
 import About from './pages/About'
 import Game from './pages/Game'
-
 
 
 
