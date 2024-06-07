00f56c5c920940b6c66a45e54dde03c24f222c2a
Denis Sab <denis.sabs@ymail.com>
2024-06-07 11:57:29 +0200

    first merge on front end completed and fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
-
 import './App.css'
 import { Routes, Route } from 'react-router-dom'
-import PokeList from './pages/PokeList'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Home from './Pages/Home'
+import PokeList from './Pages/PokeList'
 import PokeDetails from './components/PokeDetails'
-import About from './pages/About'
-import Game from './pages/Game'
+import About from './Pages/About'
+import Game from './Pages/Game'
 
 
 
