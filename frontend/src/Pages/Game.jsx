2dab991c4c36c9ce65d33a7b6205db3d31edf318
rbksy <rabiaaksoy93@gmail.com>
2024-06-12 18:10:52 +0200

    pokemon revealed

--- frontend/src/Pages/Game.jsx
+++ frontend/src/Pages/Game.jsx
@@ -1,25 +1,21 @@
-
 import { Link } from 'react-router-dom';
 
-
-
 function Game() {
-  
-
   return (
-    <div className="text-center">
-      <header className="my-8">
-        <h1 className='text-4xl font-bold mb-4'>Pokemon Games</h1>
-        <div className='space-x-4'>
+    <div className="text-center py-8">
+      <div className="my-8 max-w-4xl mx-auto">
+        <h1 className='text-5xl font-bold mb-4 text-gray-800'>
+          Pokemon Games
+        </h1>
+        <div className='flex justify-center space-x-10 m-10'>
           <Link to="/game/pokeboard">
-            <button className="bg-gray-600 text-white py-2 px-4 rounded hover:bg-blue-700 transition">Poke Board</button>
+            <button className="bg-yellow-500 text-gray-800 py-3 px-10 hover:bg-red-600 hover:text-white transform transition duration-300 ease-in-out hover:scale-105">Poke Board</button>
           </Link>
           <Link to="/game/pokefight">
-            <button className="bg-gray-600 text-white py-2 px-4 rounded hover:bg-green-700 transition">Poke Fight</button>
+          <button className="bg-blue-600 text-gray-800 py-3 px-10 hover:bg-red-600 hover:text-white transform transition duration-300 ease-in-out hover:scale-105">Poke Fight</button>
           </Link>
         </div>
-      </header>
-      
+      </div>
     </div>
   );
 }
